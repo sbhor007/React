f90a9edc8cfbebe8e97e1be90894261b7cf3e4fa
Santosh Bhor <sbhor747@gmail.com>
2024-05-06 21:08:18 +0530

    .

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -6,16 +6,21 @@ import Card from './components/card'
 
 function App() {
   const [count, setCount] = useState(0);
+  let myObj = {
+    username : 'santosh',
+    age : 20
+  }
 
+  let newArr= [1,2,3]
   return (
     <>
       <h1 className="bg-green-400 text-black p-3 rounded-md font-serif mb-5">
         Teilwind Test
       </h1>
+      <Card username = 'chai aur code' btnText='Visit me' someObj = {myObj} myarr = {newArr} /> {/* //pass values one component to another component */}
+      <Card username= 'Santosh' btnText='click me'/>
       <Card />
-      <Card />
-      <Card />
-   
+      
     </>
   );
 }
