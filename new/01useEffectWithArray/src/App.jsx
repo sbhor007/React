acea327088f31a25776a05f1af476a5b95245ca3
Santosh Bhor <sbhor747@gmail.com>
2024-05-16 01:02:56 +0530

    .

--- new/01useEffectWithArray/src/App.jsx
+++ new/01useEffectWithArray/src/App.jsx
@@ -1,32 +1,44 @@
-import React,{ useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Box from "./componunent/Box";
 
 function App() {
   const [boxContent, setBoxContent] = useState([]);
-useEffect(()=>{
-  const newBoxContain = [
-    {
-      name:'Dipak',
-      age:21
-    },
-    {
-      name:'santosh',
-      age:21
-    },
-    {
-      name:'yash',
-      age:21
-    }
-  ];
+  useEffect(() => {
+    const newBoxContain = [
+      {
+        name: "Dipak",
+        age: 21,
+      },
+      {
+        name: "santosh",
+        age: 21,
+      },
+      {
+        name: "yash",
+        age: 21,
+      },
+    ];
 
-  setBoxContent(newBoxContain)
-},[])
-  
+    setBoxContent(newBoxContain);
+  }, []);
+
+  const clearBox = () =>{
+    setBoxContent([])
+  }
+
   return (
     <>
-      {boxContent.map((curVal,index)=>{
-        return <Box key={index} name={curVal.name} age={curVal.age} />
+      {boxContent.map((curVal, index) => {
+        return <Box key={index} name={curVal.name} age={curVal.age} />;
       })}
+      <div className="flex justify-center mt-8">
+        <button
+          className="bg-green-600 px-5 py-2 rounded-md shadow-md text-white"
+          onClick={clearBox}
+        >
+          Clear
+        </button>
+      </div>
     </>
   );
 }
