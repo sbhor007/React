17b046a22df09353d7697de86eb15ed7d63081b3
Santosh Bhor <sbhor747@gmail.com>
2024-05-25 22:06:29 +0530

    .

--- new/todo/src/components/Input/Input.jsx
+++ new/todo/src/components/Input/Input.jsx
@@ -10,10 +10,10 @@ function Input({inputVal,setInput,addTodo}) {
         value={inputVal}
         onChange={setInput}
       />
-      <button className="px-11 py-3 bg-green-700 rounded-r-md outline-none"
+      <button className="px-11 py-3 bg-green-700 rounded-r-md outline-none text-xl font-semibold hover:bg-orange-600 transition duration-150 ease-out hover:ease-in"
       onClick={addTodo}
       >
-        +
+        Add
       </button>
     </div>
   );
