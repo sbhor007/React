b072784bd7c49dedb6b1db5c76672c13b6dfb81a
Santosh Bhor <sbhor747@gmail.com>
2024-05-26 23:37:28 +0530

    learn about api Calls

--- new/imdb-clone/src/components/MovieCard.jsx
+++ new/imdb-clone/src/components/MovieCard.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 
-function MovieCard() {
+function MovieCard({poster_path,name}) {
   return (
     <div
-      className="h-[40vh] w-[190px] bg-center bg-cover rounded-xl hover:cursor-pointer duration-300 hover:scale-110 flex flex-wrap flex-row justify-between items-end "
+      className="h-[50vh] w-[180px] bg-center bg-cover rounded-xl hover:cursor-pointer duration-300 hover:scale-110 flex flex-wrap flex-row justify-between items-end "
       style={{
-        backgroundImage: `url(https://wallpapercave.com/dwp1x/wp8116312.jpg)`,
+        backgroundImage: `url(https://image.tmdb.org/t/p/original/${poster_path})`,
       }}
-    ></div>
+    >
+      <div className="text-white text-xl w-full p-2 text-center bg-gray-900/60">
+        {name}
+      </div>
+    </div>
   );
 }
 
--- new/imdb-clone/src/components/Movies.jsx
+++ new/imdb-clone/src/components/Movies.jsx
@@ -1,23 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MovieCard from './MovieCard'
+import axios from 'axios'
 
 function Movies() {
+
+
+  const [movies,setMovies] = useState([])
+
+  useEffect(()=>{
+    axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=6a930fef16d1373625057b0a1806ebb9&language=en-US&page=1`)
+    .then((res)=>{
+      setMovies(res.data.results);
+      console.log(res.data.results);
+    })
+  },[])
+
   return (
     <div className='p-5'>
       <div className='text-2xl font-bold text-center m-5'>
         Trending Movies
       </div>
-      <div className='flex flex-wrap flex-row justify-between items-end '>
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
+      <div className='flex flex-wrap flex-row justify-around gap-8'>
+        {
+          movies.map((movieObj) =>{
+            return <MovieCard poster_path={movieObj.poster_path} name={movieObj.original_title}/>
+          })
+        }
+        
       </div>
     </div>
   )
 }
 
 export default  Movies
+// https://api.themoviedb.org/3/discover/movie?api_key=6a930fef16d1373625057b0a1806ebb9&language=en-US&page=2
