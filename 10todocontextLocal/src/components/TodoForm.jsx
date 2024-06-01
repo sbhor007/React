e34619dc46d79d6136cd4def766ad30c117caec7
Santosh Bhor <sbhor747@gmail.com>
2024-06-01 20:17:58 +0530

    .

--- 10todocontextLocal/src/components/TodoForm.jsx
+++ 10todocontextLocal/src/components/TodoForm.jsx
@@ -1,36 +1,39 @@
-import React, { useState } from 'react'
-import { useTodo } from '../contexts';
+import React, { useState } from "react";
+import { useTodo } from "../contexts";
 
 function TodoForm() {
-    const [todo,setTodos] = useState("")
-    const {addTodo} = useTodo()
+  const [todo, setTodos] = useState("");
+  const { addTodo } = useTodo();
 
-    const add = (e) =>{
-        e.preventDefault()
+  const add = (e) => {
+    e.preventDefault();
 
-        if(!todo) return;
+    if (!todo) return;
 
-        addTodo({todo:todo,completed:false})
-        // addTodo({id: Date.now(),todo:todo,completed:false})
-        // addTodo({todo,completed:false})
-        setTodos("")
-    }
-    console.log();
+    addTodo({ todo: todo, completed: false });
+    // addTodo({id: Date.now(),todo:todo,completed:false})
+    // addTodo({todo,completed:false})
+    setTodos("");
+  };
+  console.log();
 
-    return (
-        <form onSubmit={add}  className="flex">
-            <input
-                type="text"
-                placeholder="Write Todo..."
-                className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
-                value={todo}
-                onChange={(e) =>setTodos(e.target.value)}
-            />
-            <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
-                Add
-            </button>
-        </form>
-    );
+  return (
+    <form onSubmit={add} className="flex">
+      <input
+        type="text"
+        placeholder="Write Todo..."
+        className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+        value={todo}
+        onChange={(e) => setTodos(e.target.value)}
+      />
+      <button
+        type="submit"
+        className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0"
+      >
+        Add
+      </button>
+    </form>
+  );
 }
 
 export default TodoForm;
